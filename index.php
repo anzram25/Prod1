c9c8c41f6ddd43340108861a64f6a511d5af0465
José Francisco Anzaldo Ramírez <78508884+anzram25@users.noreply.github.com>
2022-08-16 16:55:29 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-header("Content-Type: image/png");
-$im = @imagecreate(110, 20)
-    or die("Cannot Initialize new GD image stream");
-$color_fondo = imagecolorallocate($im, 0, 0, 0);
-$color_texto = imagecolorallocate($im, 233, 14, 91);
-imagestring($im, 1, 5, 5,  "A Simple Text String", $color_texto);
-imagepng($im);
-imagedestroy($im);      
+	//http://localhost/producto1/index.php?ancho=900&alto=300&um=15
+	header("Content-type: image/png");
+	require_once("Punto.php");
+	require_once("Vista.php");
+	require_once("Controlador.php");
+	$v = new Vista($_GET['ancho'], $_GET['alto'], $_GET['um']);
+	$c = new Controlador();
+	$c->exhibir($v); 
 ?>
